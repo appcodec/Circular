7e20c700648dfd52171c6553a585cd17ce891ce2
Codec <czmajor@gmail.com>
2013-07-01 13:09:50 +0700

    Initial Commit

---
+++ Circular/AppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  AppDelegate.h
+//  Circular
+//
+//  Created by Codec on 7/1/56 BE.
+//  Copyright (c) 2556 Codec. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
