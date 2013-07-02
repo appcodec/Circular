f03c0c4b631671ee20f8cc92f2c3edf12d6db407
Codec <czmajor@gmail.com>
2013-07-02 18:00:48 +0700

    Add shadow

--- Circular/NavigationTopViewController.m
+++ Circular/NavigationTopViewController.m
@@ -12,13 +12,17 @@
 
 - (void)viewWillAppear:(BOOL)animated
 {
-  [super viewWillAppear:animated];
-  
-  if (![self.slidingViewController.underLeftViewController isKindOfClass:[MenuViewController class]]) {
-    self.slidingViewController.underLeftViewController  = [self.storyboard instantiateViewControllerWithIdentifier:@"Menu"];
-  }
-  
-  [self.view addGestureRecognizer:self.slidingViewController.panGesture];
+    [super viewWillAppear:animated];
+    
+    self.view.layer.shadowOpacity = 0.75f;
+    self.view.layer.shadowRadius = 10.0f;
+    self.view.layer.shadowColor = [UIColor blackColor].CGColor;
+    
+    if (![self.slidingViewController.underLeftViewController isKindOfClass:[MenuViewController class]]) {
+        self.slidingViewController.underLeftViewController  = [self.storyboard instantiateViewControllerWithIdentifier:@"Menu"];
+    }
+    
+    [self.view addGestureRecognizer:self.slidingViewController.panGesture];
 }
 
 @end
